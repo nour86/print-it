57cb0ccf8223e7cf4b18a6a7e11601a6e51c11d2
Nour Alchami <n.alchami@gmail.com>
2023-09-25 14:34:52 +0200

    eventlistners ajoutés

--- assets/script.js
+++ assets/script.js
@@ -16,3 +16,61 @@ const slides = [
 		"tagLine":"Autocollants <span>avec découpe laser sur mesure</span>"
 	}
 ]
+
+
+let n=slides.length;
+let i=4;
+let arrowRight=document.querySelector(".arrow_right");
+let bannerImg=document.querySelector(".banner-img");
+let bannerTagLine=document.getElementById("#banner");
+
+
+// création des dots //
+let dots=document.querySelector(".dots");
+//
+
+
+let arrowLeft=document.querySelector(".arrow_left");
+
+// left click
+arrowLeft.addEventListener("click", slideLeft ()) {
+	console.log("Vous avez cliqué sur la fleche gauche")
+};
+
+function slideLeft (index) {
+	i=i-1;
+	
+
+};
+
+
+// right click
+
+arrowLeft.oncontextmenu= function () {
+	console.log("right click");
+	return false; //désactive le right click par défaut
+}
+
+document.addEventListener('keypress', (event) => {
+    console.log(event.key);
+});
+
+
+
+let div=`
+	<div class="dot"></div>
+	<div class="dot"></div>
+	<div class="dot dot_selected"></div>
+	`;
+
+dots.innerHTML=div;
+
+
+//changer l'image de fond //
+bannerImg.setAttribute("src","./assets/images/slideshow/slide2.jpg");
+
+// bannerTagLine.innerHTML("test");
+
+
+
+console.log(dots);
